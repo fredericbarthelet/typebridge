7eada3fd3abbdb52c5899bd7a4d030e89d00002a
Antoine Sion <54913600+antoinesion@users.noreply.github.com>
2021-10-11 14:59:45 +0200

    chunk entries correctly (#9)

--- src/Bus.test.ts
+++ src/Bus.test.ts
@@ -1,4 +1,40 @@
-import { computeEventSize } from './Bus';
+import { chunkEntries, computeEventSize } from './Bus';
+
+const entry = {
+  Detail: 'small'.repeat(Math.round(50000 / 5)), // ~ 50Kb
+};
+
+const cases = [
+  {
+    name: 'no entry',
+    entries: [],
+    output: [],
+  },
+  {
+    name: 'one small enough entry',
+    entries: [entry],
+    output: [[entry]],
+  },
+  {
+    name: '5 small enough entries',
+    entries: new Array(5).fill(entry),
+    output: [new Array(5).fill(entry)],
+  },
+  {
+    name: '6 entries that exceeds size limit',
+    entries: new Array(6).fill(entry),
+    output: [new Array(5).fill(entry), [entry]],
+  },
+  {
+    name: '12 entries that exceeds size limit twice',
+    entries: new Array(12).fill(entry),
+    output: [
+      new Array(5).fill(entry),
+      new Array(5).fill(entry),
+      [entry, entry],
+    ],
+  },
+];
 
 describe('Bus', () => {
   describe('#computeEventSize', () => {
@@ -10,5 +46,14 @@ describe('Bus', () => {
         }),
       ).toBe(27);
     });
+  });
+
+  describe('#chunkEntries', () => {
+    it.each(cases)(
+      'should return the chunks array for $name',
+      ({ entries, output }) => {
+        expect(chunkEntries(entries)).toEqual(output);
+      },
+    );
   });
 });
